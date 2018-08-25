9910ffcc03d2070fb5a2b6bd8a5fc7e562853e93
Roscoe Coney <scoe@Roscoes-MacBook-Pro.local>
2018-08-25 15:08:52 -0700

    adds webpackand reactJS

---
+++ data/dummyData.js
@@ -0,0 +1,1 @@
+var array = ['milk', 'peanuts', 'pineapple'];
---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log('you got this!')
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+  return <div>Let's do it! -from ReactJS</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
